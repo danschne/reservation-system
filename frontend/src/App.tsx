9f6b4ed48f34af5590e940ac5eff2296c1322c10
Daniel Schneider <schneider.daniel92@web.de>
2021-03-10 13:59:08 +0100

    set up eslint; added basic shape of the reservation form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { BaseSyntheticEvent, useState } from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
+  const [numberOfPersons, setNumberOfPersons] = useState(1);
+  
+  function adjustNumberOfPersons(e: BaseSyntheticEvent): void {
+    setNumberOfPersons(e.currentTarget.value as number);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col lg={{ offset: 3, span: 6 }}>
+          <Row>
+            <Col>
+              <h3 className="text-center">Reservierungssystem</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form>
+                <Form.Group as={Row} controlId="formReservationName">
+                  <Col>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" value={undefined} required />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row} controlId="formReservationDate">
+                  <Col>
+                    <Form.Label>Datum</Form.Label>
+                    <Form.Control type="date" value={undefined} required />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row} controlId="formReservationNumberOfPersons">
+                  <Col>
+                    <Form.Label>Anzahl Personen</Form.Label>
+                    <Form.Control type="number" required min={1} max={12}
+                                  value={numberOfPersons} onChange={adjustNumberOfPersons} />
+                  </Col>
+                </Form.Group>
+                <Button type="submit" block>
+                  Reservieren
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
